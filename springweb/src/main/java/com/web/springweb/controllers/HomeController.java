a67e85093b4b6a8536a770ca4e150041e34cb5ca
Carlos-henri <carlinhos4547@outlook.com>
2023-07-27 13:56:52 -0300

    Iniciando aplicação Web

---
+++ springweb/src/main/java/com/web/springweb/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.web.springweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(Model model){
+
+        model.addAttribute("nome", "Douglas");
+
+        return "home/index";
+    }
+}
